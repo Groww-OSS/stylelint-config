758524ffc2181840cdfce190d6b0ea6b26ecf8b4
Rishav Thakur <rishav.21m@gmail.com>
2021-06-16 10:33:29 +0530

    feat: added no-unsup-browser-css

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ module.exports = {
     "declaration-no-important": [true, { "severity": "warning" }],
     "rule-empty-line-before": ["always", { "severity": "warning" }],
     "color-hex-case": ["upper", { "severity": "warning" }],
-    "sh-waqar/declaration-use-variable": [["color", "background","background-color" ,"border", "z-index"], { "severity": "warning" }],
+    "sh-waqar/declaration-use-variable": [["color", "background", "background-color", "border", "z-index"], { "severity": "warning" }],
+    "plugin/no-unsupported-browser-features": [true, {
+      "browsers": ["> 1%", "Last 2 versions"],
+      "ignore": ["rem"],
+      "ignorePartialSupport": true,
+      "severity": "warning"
+    }]
   }
 }
